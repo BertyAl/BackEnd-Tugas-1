425744d0eefff9a968721b248cea2a8567b49fe8
Albert <131771057+BertyAl@users.noreply.github.com>
2024-05-03 22:24:28 +0700

    Delete BackEnd-Tugas-1-master directory

--- BackEnd-Tugas-1-master/Backend/models/Form.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-const FormSchema = mongoose.Schema({
-  title: String,
-  thread: String,
-})
-
-module.exports = Form = mongoose.model("Community", FormSchema,"Community");
--- BackEnd-Tugas-1-master/Backend/models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const UserSchema = mongoose.Schema({
-  username: String,
-  email: String,
-  password: String
-})
-
-module.exports = User = mongoose.model("Account", UserSchema,"Account");
